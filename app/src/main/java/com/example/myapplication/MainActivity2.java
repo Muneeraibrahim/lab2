8d7f2d53a72785769c58bf3b9ac3f9d475ae9181
Muneeraibrahim <memealarifi@gmail.com>
2023-03-28 15:11:21 +0300

    lab2

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        String s = i.getStringExtra( "personName") ;
+        TextView x = findViewById(R.id.msg);
+        x.setText("Hello"+s+"!");
+    }
+}
